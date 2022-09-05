046ebae9a6ccd50019c3ac0155fd9374b62545b2
milesr100 <100169944+milesr100@users.noreply.github.com>
2022-09-05 18:10:08 -0400

    Add files via upload

---
+++ flaskapp1.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>This is the about page!</h1>'
+
+@app.route('/contact')
+def contact():
+    return '<h1>COntact me at...</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8000)
+
